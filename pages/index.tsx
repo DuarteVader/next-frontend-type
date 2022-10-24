58d953b26ec393a5cf594ea6e84ead18319c8878
DuarteVader <victorduarte02@hotmail.com>
2022-10-24 00:30:51 -0300

    enhancement on code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import type { NextPage } from "next";
+import React, { useState } from "react";
 import {
   Button,
   Checkbox,
   Flex,
   FormControl,
   FormLabel,
-  Heading,
   Input,
   Link,
   Stack,
@@ -15,34 +13,27 @@ import {
   InputGroup,
   Box,
   Text,
-  HStack,
   useColorModeValue,
   Image,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 
-import { motion } from 'framer-motion'
-import { useAuth } from '../hooks/useAuth'
-import backgroundImg from '../public/background.png'
-
-interface Ilogin {
-  email: string
-  password: string
-}
+import { motion } from "framer-motion";
+import { useAuth } from "../hooks/useAuth";
 
 const Auth: NextPage = () => {
-  const { Login } = useAuth()
-  const router = useRouter()
+  const { Login } = useAuth();
 
-  const [LogResponse, setLogResponse] = useState<Ilogin>({
-    email: '',
-    password: '',
-  })
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [show, setShow] = React.useState(false);
+  const [validEmail, setValidEmail] = React.useState(false);
 
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [show, setShow] = React.useState(false)
+  function validateEmail(email: string) {
+    var re = /\S+@\S+\.\S+/;
+    return setValidEmail(re.test(email));
+  }
 
-  const handleClick = () => setShow(!show)
+  const handleClick = () => setShow(!show);
 
   return (
     <Stack
@@ -50,58 +41,63 @@ const Auth: NextPage = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.75 } }}
       exit={{ opacity: 0 }}
-      minH={'100vh'}
-      direction={{ base: 'column', md: 'row' }}
+      bgImage={"/background.jpg"}
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        width: "100%",
+      }}
+      align={"center"}
+      justify={"center"}
     >
-      <Flex flex={1}>
-        <Stack
-          bgImage={'/background.jpg'}
-          style={{
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat',
-            backgroundColor: '#000',
-            height: '100%',
-            width: '100%',
-          }}
-          align={'center'}
-          justify={'center'}
-        >
-          <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-            <Stack align={'center'}>
-              <Image src="/fenix.png" alt="logo" />
-              <Text
-                color={'#2c2a2a'}
-                style={{
-                  fontFamily: 'Inter',
-                  fontSize: '32px',
-                  fontWeight: '600',
-                  lineHeight: '28px',
-                  letterSpacing: '0em',
-                  textAlign: 'center',
-                }}
-              >
-                Sign in to your account
-              </Text>
-            </Stack>
-            <Box
-              rounded={'lg'}
-              bg={useColorModeValue('white', 'gray.700')}
-              boxShadow={'lg'}
-              p={8}
-              w={'492px'}
-            >
-              <Stack spacing={4} w={'full'} maxW={'md'}>
-                <FormControl id="email">
-                  <FormLabel>Email address</FormLabel>
-                  <Input
-                    type="email"
-                    placeholder="Enter email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </FormControl>
-                <FormLabel>Password</FormLabel>
-                <InputGroup size="md">
+      <Stack spacing={8}>
+        <Stack align={"center"}>
+          <Image src="/fenix.png" alt="logo" />
+          <Text
+            color={"#2c2a2a"}
+            style={{
+              fontFamily: "Inter",
+              fontSize: "32px",
+              fontWeight: "600",
+              lineHeight: "28px",
+              letterSpacing: "0em",
+              textAlign: "center",
+            }}
+          >
+            Sign in to your account
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}
+          w={'492px'}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input placeholder="Enter email" type="email" onChange={(e) => {
+                setEmail(e.target.value)
+                validateEmail(e.target.value)
+              }}/>
+            </FormControl>
+            {validEmail || email.length === 0 ?  false : 
+            <Text
+            color={"#f00"}
+            style={{
+              fontSize: "12px",
+              fontWeight: "400",
+              lineHeight: "10px",
+              letterSpacing: "0em",
+            }}
+          >
+            * Invalid email
+          </Text>}
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <InputGroup size="md">
                   <Input
                     pr="4.5rem"
                     type={show ? 'text' : 'password'}
@@ -114,33 +110,29 @@ const Auth: NextPage = () => {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
-                <Stack spacing={6}>
-                  <Stack
-                    direction={{ base: 'column', sm: 'row' }}
-                    align={'start'}
-                    justify={'space-between'}
-                  >
-                    <Checkbox>Remember me</Checkbox>
-                    <Link color={'blue.500'}>Forgot password?</Link>
-                  </Stack>
-                  <Button
-                    colorScheme={'blue'}
-                    variant={'solid'}
-                    onClick={() => {
-                      Login(email, password)
-                    }}
-                  >
-                    Sign in
-                  </Button>
-                </Stack>
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.400'}>Forgot password?</Link>
               </Stack>
-            </Box>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign in
+              </Button>
+            </Stack>
           </Stack>
-          {/*  */}
-        </Stack>
-      </Flex>
+        </Box>
+      </Stack>
     </Stack>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
